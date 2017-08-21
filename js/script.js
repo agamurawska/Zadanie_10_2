4d74b7fbc3acdd538003e25783c1ff1af7768576
aga <aga.murawska@gmail.com>
2017-08-22 00:07:26 +0200

    carousel

---
+++ js/script.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function(){
+	var carouselList = $("#carousel ul");
+
+	function changeSlide() {
+    carouselList.animate({'marginLeft':-500}, 500, moveFirstSlide);
+  };  
+  changeSlide();
+
+  function moveFirstSlide() {
+    var firstImg = carouselList.find("li:first");
+    	lastImg = carouselList.find("li:last");
+
+    lastImg.after(firstImg);
+    carouselList.css({marginLeft:0});
+  };
+
+	setInterval(changeSlide, 3000);		
+});
